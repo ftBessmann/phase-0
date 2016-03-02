4cbbf9c6568d98d3f4e79142df0a6879934766fe
ftBessmann <a.ll@comtechnics.by>
2016-03-02 19:37:19 +0300

    Define Math Methods

---
+++ phase-0/week-4/math/my_solution.rb
@@ -0,0 +1,20 @@
+# Math Methods
+# I worked on this challenge by myself.
+
+# My Solution Below
+
+def add(num_1, num_2)
+ return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ return num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ return num_1.to_f / num_2
+end
