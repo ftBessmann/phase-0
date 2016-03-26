3aa7a8cb3e849e4ef392139a7ec2188f32a4ddbd
ftBessmann <a.ll@comtechnics.by>
2016-03-26 23:38:20 +0300

    Olympics

--- phase-0/week-7/gps6/my_solution.rb
+++ phase-0/week-7/gps6/my_solution.rb
@@ -1,29 +1,87 @@
+
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Anna.
 # We spent [#] hours on this challenge.
-
+STATE_DATA = {
+  "Alabama" => {population_density: 94.65, population: 4822023},
+  "Alaska" => {population_density: 1.1111, population: 731449},
+  "Arizona" => {population_density: 57.05, population: 6553255},
+  "Arkansas" => {population_density: 56.43, population: 2949131},
+  "California" => {population_density: 244.2, population: 38041430},
+  "Colorado" => {population_density: 49.33, population: 5187582},
+  "Connecticut" => {population_density: 741.4, population: 3590347},
+  "Delaware" => {population_density: 470.7, population: 917092},
+  "Florida" => {population_density: 360.2, population: 19317568},
+  "Georgia" => {population_density: 172.5, population: 9919945},
+  "Hawaii" => {population_density: 216.8, population: 1392313},
+  "Idaho" => {population_density: 19.15, population: 1595728},
+  "Illinois" => {population_density: 231.9, population: 12875255},
+  "Indiana" => {population_density: 182.5, population: 6537334},
+  "Iowa" => {population_density: 54.81, population: 3074186},
+  "Kansas" => {population_density: 35.09, population: 2885905},
+  "Kentucky" => {population_density: 110.0, population: 4380415},
+  "Louisiana" => {population_density: 105.0, population: 4601893},
+  "Maine" => {population_density: 43.04, population: 1329192},
+  "Maryland" => {population_density: 606.2, population: 5884563},
+  "Massachusetts" => {population_density: 852.1, population: 6646144},
+  "Michigan" => {population_density: 174.8, population: 9883360},
+  "Minnesota" => {population_density: 67.14, population: 5379139},
+  "Mississippi" => {population_density: 63.50, population: 2984926},
+  "Missouri" => {population_density: 87.26, population: 6021988},
+  "Montana" => {population_density: 6.86, population: 1005141},
+  "Nebraska" => {population_density: 23.97, population: 1855525},
+  "Nevada" => {population_density: 24.8, population: 2758931},
+  "New Hampshire" => {population_density: 147.0, population: 1320718},
+  "New Jersey" => {population_density: 1205, population: 8864590},
+  "New Mexico" => {population_density: 17.16, population: 2085538},
+  "New York" => {population_density: 415.3, population: 19570261},
+  "North Carolina" => {population_density: 200.6, population: 9752073},
+  "North Dakota" => {population_density: 9.92, population: 699628},
+  "Ohio" => {population_density: 282.5, population: 11544225},
+  "Oklahoma" => {population_density: 55.22, population: 3814820},
+  "Oregon" => {population_density: 40.33, population: 3899353},
+  "Pennsylvania" => {population_density: 285.3, population: 12763536},
+  "Rhode Island" => {population_density: 1016, population: 1050292},
+  "South Carolina" => {population_density: 157.1, population: 4723723},
+  "South Dakota" => {population_density: 10.86, population: 833354},
+  "Tennessee" => {population_density: 156.6, population: 6456243},
+  "Texas" => {population_density: 98.07, population: 26059203},
+  "Utah" => {population_density: 34.3, population: 2855287},
+  "Vermont" => {population_density: 67.73, population: 626011},
+  "Virginia" => {population_density: 207.3, population: 8185867},
+  "Washington" => {population_density: 102.6, population: 6724540},
+  "Washington,D.C."=> {population_density: 10357, population: 632323},
+  "West Virginia" => {population_density: 77.06, population: 1855413},
+  "Wisconsin" => {population_density: 105.2, population: 5726398},
+  "Wyoming" => {population_density: 5.851, population: 576412}
+}
 # EXPLANATION OF require_relative
 #
 #
-require_relative 'state_data'
+#require_relative 'state_data'
 
 class VirusPredictor
-
+#  attr_reader :population_density, :population, :state
+  
+#Initializes instance of class with instance variable @state
+# Initialiaze method sets the attributes(properties) of the object shared by all instances of the class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+# It pull in returned (output) values from private methods predicted_deaths and speed_of_spread methods. Is a public instance method of the class, operates on instance of the class. 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths #(@population_density, @population, @state)
+    speed_of_spread #(@population_density, @state)
   end
 
+  # It turns the two methods into private, i.e. accessed only inside the class.
   private
-
-  def predicted_deaths(population_density, population, state)
+  
+  # using control flow and input data counts the number of deaths predicted for a certain state. Numbers are rounded to integers. Prints out string describing predicted deaths of state. 
+  def predicted_deaths#(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -40,8 +98,8 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
-  def speed_of_spread(population_density, state) #in months
+# Based on the population density the speed is calculated. It uses control flow to compare population densities to detemine the speed of the spread  of the virus for a certain state. Outputs a string with speed of spread of the virus. 
+  def speed_of_spread#(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -69,6 +127,12 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+def all_states(list)
+  list.each do |key,value|
+    VirusPredictor.new(key,value[:population_density], value[:population]).virus_effects
+  end 
+end
+all_states (STATE_DATA)
 
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
--- phase-0/week-7/javascript_olympics.js
+++ phase-0/week-7/javascript_olympics.js
@@ -1,33 +1,57 @@
 // JavaScript Olympics
 
-// I paired with Alec Hendrickson on this challenge.
-
-// This challenge took me [#] hours.
-
+// I paired with Alec Hendrickson on this challenge for 1 2.5 hours.
 
 // Warm Up
-
-
-
-
 // Bulk Up
-
-
-
-
+var athlete1 = {
+  name: "Daria Domracheva",
+  event: "skiing",
+};
+  
+var athlete2 = {
+  name: "Shawn White",
+  event: "snowboarding",
+};
+var athleteArray = [athlete1, athlete2]
+   
+athleteArray.forEach(function(athlete) {
+  athlete["win"] = athlete.name + " won " + athlete.event + "!";
+});
+console.log(athleteArray)
 // Jumble your words
-
-
-
-
+var string = "!sdrow ruoy elbmuJ";
+function ReverseString(string) {
+  return string.split("").reverse().join("")
+};
+console.log(ReverseString(string));
 // 2,4,6,8
-
-
-
-
+function EvensArray(value) {
+  return value%2 === 0;
+}
+var filtered = [12, 5, 8, 130, 44].filter(EvensArray);
+console.log(filtered)
 // "We built this city"
-
-
-
-
-// Reflection
+function Athlete(name, age, sport, quote){  
+  this.name = name;
+  this.age = age;
+  this.sport = sport;
+  this.quote = quote;
+};
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!");
+console.log(michaelPhelps.constructor === Athlete);
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote);
+/* Reflection
+1. What JavaScript knowledge did you solidify in this challenge?
+I solidified knowledge about objects, functions and built-in methods to manipulate data in arrays.
+2. What are constructor functions?
+Constructor functions are functions used to create many objects of one type.
+3. How are constructors different from Ruby classes (in your research)?
+Ruby's class is a special kind of object that creates new instances.
+JMeanwhile, avaScript instances are created with a constructor.
+Nevertheless, there is no special kind of thing that constructs new objects, every function is (potentially) a constructor.
+JavaScript doesn’t have a special syntax or special kind of object to define the behaviour of instances, it has “prototypes”.
+Prototypes are not a special kind of object, they’re just objects.
+Instance behaviour in JavaScript is defined by modifying the prototype directly, e.g. by adding functions to it as properties.
+There is no special syntax for defining a class or modifying a class.
+*/
